e804a81f8673d6d22dfb94aa27c4e3357ed764b5
Oksana Bashynska <oksanavvisotska@gmail.com>
2021-05-05 14:40:41 +0300

    8 задача

--- js/task-08.js
+++ js/task-08.js
@@ -3,10 +3,27 @@ const renderBtn = document.querySelector('[data-action="render"]');
 const destroyBtn = document.querySelector('[data-action="destroy"]');
 const boxesContainer = document.getElementById('boxes');
 
-renderBtn.addEventListener('click', createBoxes);
-function createBoxes(amount) {
+//
+//input.addEventListener('input', onInputGetAmount);
+ renderBtn.addEventListener('click', createBoxes);
+
+/* function onInputGetAmount(event) {
+    const amount = event.currentTarget.value;
+    const amountNumb = Number(amount);
+} */
+const amountNumb = 7;
+   
+    function createBoxes (amountNumb) {
     
-}
+        for (let i = 0; i < amountNumb; i++) {
+            const ingredientsItemEl = document.createElement('div');
+            boxesContainer.appendChild(ingredientsItemEl);
+        }
+ 
+    }
+   
+
+
 
 //Пока не знаю
 
